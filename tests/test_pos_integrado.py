2a5f7b73d2fe06f332ed42f5a50aa6df7bd14003
Matias Vasquez <m.vasquezac@gmail.com>
2023-07-31 13:48:02 -0400

    test: add poll exception test

--- tests/test_pos_integrado.py
+++ tests/test_pos_integrado.py
@@ -26,3 +26,9 @@ class TestPosIntegrado(unittest.TestCase):
         self.mock_serial.read_data.return_value = self.ACK
         result = self.pos.poll()
         self.assertEqual(True, result)
+
+    def test_poll_exception(self):
+        self.mock_serial.read_data.side_effect = self.mock_exception
+        with self.assertRaises(TransbankException) as context:
+            self.pos.poll()
+        self.assertTrue('Unable to send Poll' in str(context.exception))
